baafc9e0d03d8dbde319b818b329eea2eac82bab
Monzurul Islam <Xatta-Trone@users.noreply.github.com>
2022-11-15 03:54:45 +0900

    data type

---
+++ test.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println("test")
+
+	// variables 
+
+	var name string = "xatta";
+	var name2 = "xatta";
+	name3 := "asdf";
+
+	var num int8 = 127;
+
+
+	println(name);
+	println(name2);
+	println(name3);
+	println(num);
+}
