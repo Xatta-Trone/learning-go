e13915959b29305c5dc151df0a74d34c26a3ca85
Monzurul Islam <Xatta-Trone@users.noreply.github.com>
2022-11-15 04:26:54 +0900

    array and slices

--- test.go
+++ test.go
@@ -1,22 +1,58 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 
-	fmt.Println("test")
+	// fmt.Println("test")
 
-	// variables 
+	// // variables
 
-	var name string = "xatta";
-	var name2 = "xatta";
-	name3 := "asdf";
+	// var name string = "xatta \n";
+	// var name2 = "xatta";
+	// name3 := "asdf";
 
-	var num int8 = 127;
+	// var num int8 = 127;
 
+	// println(name);
+	// println(name2);
+	// println(name3);
+	// println(num);
 
-	println(name);
-	println(name2);
-	println(name3);
-	println(num);
+	// fmt.Print(name,name2)
+
+	// fmt.Printf("%s %d",name2,num)
+
+	// // arrays
+
+	// var ages [3]int = [3]int{10, 12, 13}
+
+	// names := []string{"asdf", "asdf"}
+
+	// fmt.Println(ages, len(ages))
+	// fmt.Println(names, len(names))
+
+	// // // slices
+
+	// var scores = []int{10, 12, 13}
+	// scores[0] = 100
+
+	// // append
+	// scores = append(scores, 100)
+
+	// fmt.Println(scores)
+
+	// // slice range
+
+	// rangeOne := scores[1:3]
+
+	// fmt.Println(rangeOne)
+
+	greet := "my name is xatta"
+
+	fmt.Println(strings.Contains(greet,"xatta"))
+
 }
